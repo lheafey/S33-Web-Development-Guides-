7effbe5f3c36ac3aa17591e57e4c7b7496953499
Jackson Quinn <127517426+j-quinn01@users.noreply.github.com>
2024-05-12 21:16:18 -0400

    CSS section draft

--- full-page/src/App.js
+++ full-page/src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import GitHub from './GitHub';
+import DescriptionCss from './CSS';
 
 
 function App() {
   return (
     <div className="App">
      <h1>Github Infomation</h1>
-     <GitHub/>
+     <GitHub />
+     <h1>CSS Information</h1>
+     <DescriptionCss />
+
     </div>
+
   );
 }
 
